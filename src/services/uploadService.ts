e9dac9794d20b17c9148cbf68dd822f7edae86d1
MrWinRock <mrwinrock11@gmail.com>
2024-10-06 16:18:51 +0700

    update: upload and use profile picture

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,11 +1,16 @@
+// src/services/uploadService.ts
 import api from "./api";
 
-export const uploadService = async (imageFile: File): Promise<string> => {
+export const uploadService = async (
+  imageFile: File,
+  userId: number
+): Promise<string> => {
   const formData = new FormData();
   formData.append("image", imageFile);
+  formData.append("userId", userId.toString());
 
   try {
-    const response = await api.post("/upload", formData, {
+    const response = await api.post("/upload/profile-picture", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -13,6 +18,17 @@ export const uploadService = async (imageFile: File): Promise<string> => {
     return response.data.imageUrl;
   } catch (error) {
     console.error("Error uploading image: ", error);
+    throw error;
+  }
+};
+
+export const getProfilePicture = async (userId: number): Promise<string> => {
+  try {
+    const response = await api.get(`/upload/${userId}/profile-picture`);
+
+    return response.data.profile_picture_url;
+  } catch (error) {
+    console.error("Error fetching profile picture:", error);
     throw error;
   }
 };
